f9adb9cad6f1406a9c1ece2acccfde482614fd12
chenzhiduo <zhiduochen@gmail.com>
2017-04-05 16:53:51 +0800

    implement movies#edit

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,6 +7,10 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
   end
 
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
   def new
     @movie = Movie.new
   end
