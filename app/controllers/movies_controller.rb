89d74784c71724dc6f07c2087e63755c993fdfe5
chenzhiduo <zhiduochen@gmail.com>
2017-04-05 17:50:38 +0800

    add user_id to movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -18,6 +18,7 @@ class MoviesController < ApplicationController
 
   def create
     @movie = Movie.new(movie_params)
+    @movie.user = current_user
     if @movie.save
        redirect_to movies_path
     else
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,4 @@
 class Movie < ApplicationRecord
+  belongs_to :user
   validates :title, :description, presence: true
 end
