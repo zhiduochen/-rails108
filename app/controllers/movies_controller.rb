d19f0638492437b371a7f78608f8c50e4bf40bab
chenzhiduo <zhiduochen@gmail.com>
2017-04-05 16:58:47 +0800

    implement movies#destroy

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -31,6 +31,12 @@ class MoviesController < ApplicationController
     end
   end
 
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_path, alert: 'Movie delete'
+  end
+
   private
 
   def movie_params
