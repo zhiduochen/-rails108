e28362eabed89efe4a91a043df9687be15e32eba
chenzhiduo <zhiduochen@gmail.com>
2017-04-05 18:10:50 +0800

    use before_action to find_movie_and_check_permission

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,6 @@
 class MoviesController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_movie_and_check_permission, only: [:edit, :update, :destroy]
   def index
     @movies = Movie.all
   end
@@ -9,10 +10,6 @@ class MoviesController < ApplicationController
   end
 
   def edit
-    @movie = Movie.find(params[:id])
-    if current_user != @movie.user
-      redirect_to root_path, alert: "You have no permission."
-    end
   end
 
   def new
@@ -30,10 +27,6 @@ class MoviesController < ApplicationController
   end
 
   def update
-    @movie = Movie.find(params[:id])
-    if current_user != @movie.user
-      redirect_to root_path, alert: "You have no permission."
-    end
     if @movie.update(movie_params)
       redirect_to movies_path, notice: 'Update Success'
     else
@@ -42,17 +35,19 @@ class MoviesController < ApplicationController
   end
 
   def destroy
-    @movie = Movie.find(params[:id])
-
-    if current_user != @movie.user
-      redirect_to root_path, alert: "You have no permission."
-    end
-    
     @movie.destroy
     redirect_to movies_path, alert: 'Movie delete'
   end
 
   private
+
+  def find_movie_and_check_permission
+    @movie = movie.find(params[:id])
+
+    if current_user != @movie.user
+      redirect_to root_path, alert: "You have no permission."
+    end
+  end
 
   def movie_params
     params.require(:movie).permit(:title, :description)
